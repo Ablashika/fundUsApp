3f4cb1e01b8227f2b42b9afb663ecb1847b30a3b
kingsfieldakp <58221388+kingsfieldakp@users.noreply.github.com>
2021-04-12 18:40:31 +0000

    added 4 new screens

---
+++ src/screens/ThankYouScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { LottieView } from "lottie-react-native";
+
+
+export default class ThankYouScreen extends Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <View
+                
+                style={{
+                    
+                    backgroundColor: 'white'
+                }}
+            >
+                <Text>OBEYE OBEYIE</Text>
+                <LottieView
+                    source={require('../assets/splash.json')}
+                    autoPlay
+                    loop={false}
+                    speed={0.5}
+                    onAnimationFinish={() => {
+                        console.log('Animation Finished!')
+                        this.props.navigation.replace('Home');
+                    }}
+                />
+            </View>
+        )
+    }
+}
