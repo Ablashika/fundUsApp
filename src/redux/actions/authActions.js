9fbdf97c50f290161a56c0227bde62be4a9ab407
Ablashika <kofoworola203@gmail.com>
2021-05-04 18:01:30 +0000

    new changes

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,71 +1,64 @@
-import firebase from '../../Firebase/firebase';
+import firebase from "../../firebase/firebase";
 
 export function createEmailAccount(email, password) {
-    return async (dispatch) => {
-        try {
-            const user = await firebase
-                .auth()
-                .createUserWithEmailAndPassword(email, password)
-            console.log(user)
-            dispatch(loggedIn(user))
-        } catch (error) {
-            dispatch(SignUpError(error.message))
-        }
+  return async (dispatch) => {
+    try {
+      const user = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      console.log(user);
+      dispatch(loggedIn(user));
+    } catch (error) {
+      dispatch(SignUpError(error.message));
     }
-        
+  };
 }
 
 export function loginEmailAccount(email, password) {
-    return async (dispatch) => {
-        try {
-            const user = await firebase
-                .auth()
-                .signInWithEmailAndPassword(email, password)
-            console.log(user )
-            dispatch(loggedIn(user))
-        } catch (error) {
-            dispatch (loginError(error.message))
-        } 
+  return async (dispatch) => {
+    try {
+      const user = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+      console.log(user);
+      dispatch(loggedIn(user));
+    } catch (error) {
+      dispatch(loginError(error.message));
     }
-    
+  };
 }
 
 export function logout() {
-    return async (dispatch) => {
-        try {
-           await firebase.auth() .signOut()
-           dispatch(loggedOut()) 
-        } catch (error) {
-            
-        }
-    }
-    
+  return async (dispatch) => {
+    try {
+      await firebase.auth().signOut();
+      dispatch(loggedOut());
+    } catch (error) {}
+  };
 }
 
 function loggedIn(user) {
-    return {
-        type: "LOGGED_IN",
-        payload:user
-    }
+  return {
+    type: "LOGGED_IN",
+    payload: user,
+  };
 }
-
 
 function loggedOut() {
-    return {
-        type: "LOGGED_OUT "
-        
-    }
+  return {
+    type: "LOGGED_OUT ",
+  };
 }
 export function SignUpError(error) {
-    return {
-        type: "SIGNUP_ERROR",
-        payload:error
-    }
+  return {
+    type: "SIGNUP_ERROR",
+    payload: error,
+  };
 }
 
 export function loginError(error) {
-    return {
-        type: "LOGIN_ERROR",
-        payload:error
-    }
+  return {
+    type: "LOGIN_ERROR",
+    payload: error,
+  };
 }
